0196168ffd8cb5638d1c59d8a2376aa2d0dbd3be
Thomas Peyruse <thomas.peyruse@gmail.com>
2019-06-16 08:25:28 +0200

    Version fonctionnelle sur moteur 1

---
+++ MVCA2/MVCA2.ino
@@ -0,0 +1,249 @@
+#include <Wire.h>
+#include <VL53L1X.h>
+
+// encoder
+#define CHA1 2
+#define CHB1 4
+#define CHA2 3
+#define CHB2 5
+#define CHA3 18
+#define CHB3 22
+#define CHA4 19
+#define CHB4 24
+// I²C on 20 and 21
+
+int pinPWM[] =    {10, 11, 12, 13};
+int pinDir[] =    {30, 31, 32, 33};
+int SHUTLASER[] = {6 , 7, 8, 9};
+
+int realPos[] =     {0, 0, 0, 0};
+int desiredPos[] =  {0, 0, 0, 0};
+int desiredVit[] =  {0, 0, 0, 0};
+int rangemm[] =     {0, 0, 0, 0};
+int state[] =       {0, 0, 0, 0};
+
+// serial decoder
+int serialState = 0;
+String inputString = "";
+int id;
+int mode;
+int param;
+int temp;
+int error;
+int errorInt;
+
+VL53L1X lasers[4];
+
+// sampling rate (ms)
+int dtMs = 10;
+int Vmax = 100;
+int Kp = 1;
+float Ki = 0.0;
+int freq;
+
+void setup() {
+  // encoder pins in input:
+  pinMode(CHA1, INPUT_PULLUP);
+  pinMode(CHB1, INPUT_PULLUP);
+  pinMode(CHA2, INPUT_PULLUP);
+  pinMode(CHB2, INPUT_PULLUP);
+  pinMode(CHA3, INPUT_PULLUP);
+  pinMode(CHB3, INPUT_PULLUP);
+  pinMode(CHA4, INPUT_PULLUP);
+  pinMode(CHB4, INPUT_PULLUP);
+
+  // motors switched off
+  for (int imotor = 0; imotor <= 3; imotor++) {
+    pinMode(pinPWM[imotor], OUTPUT);
+    analogWrite(pinPWM[imotor], 0);
+    pinMode(pinDir[imotor], OUTPUT);
+    digitalWrite(pinDir[imotor], LOW);
+  }
+
+  //trigger interrupt on channel A
+  attachInterrupt(digitalPinToInterrupt(CHA1), incDec1, RISING);
+  attachInterrupt(digitalPinToInterrupt(CHA2), incDec2, RISING);
+  attachInterrupt(digitalPinToInterrupt(CHA3), incDec3, RISING);
+  attachInterrupt(digitalPinToInterrupt(CHA4), incDec4, RISING);
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000);
+  // switch off all lasers
+  for (int ilaser = 0; ilaser <= 3; ilaser++) {
+    pinMode(SHUTLASER[ilaser], OUTPUT);
+    digitalWrite(SHUTLASER[ilaser], LOW);
+  }
+  // set address of lasers
+  for (int ilaser = 0; ilaser <= 3; ilaser++) {
+    digitalWrite(SHUTLASER[ilaser], HIGH);
+    lasers[ilaser].init();
+    lasers[ilaser].setAddress(ilaser + 1);
+    lasers[ilaser].setDistanceMode(VL53L1X::Short);
+    lasers[ilaser].setMeasurementTimingBudget(20000);
+    lasers[ilaser].startContinuous(20);
+    delay(100);
+  }
+}
+
+void loop() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      /*Serial.print("etat:");
+      Serial.println(serialState);
+      Serial.print("entree:");
+      Serial.println(inputString);*/
+      switch (serialState) {
+        case 0:
+          if (inputString == "A") {
+            serialState = 1;
+          }
+          break;
+        case 1:
+          temp = inputString.toInt();
+          if (temp == 0) {
+            serialState = 0;
+          } else {
+            id = temp;
+            //Serial.print("id:");
+            //Serial.println(id);
+            serialState = 2;
+          }
+          break;
+        case 2:
+          temp = inputString.toInt();
+          if (temp == 0) {
+            serialState = 0;
+          } else {
+            mode = temp;
+            //Serial.print("mode:");
+            //Serial.println(mode);
+            serialState = 3;
+          }
+          break;
+        case 3:
+          param = inputString.toInt();
+          if (id <= 4) {
+            state[id - 1] = mode;
+            if (mode == 1) {
+              realPos[id - 1] = param;
+            } else if (mode == 2) {
+              desiredVit[id - 1] = param;
+            } else if (mode == 3) {
+              errorInt = 0;
+              desiredPos[id - 1] = param;
+            }
+          } else if (id == 5) {
+            Vmax = param;
+          } else if (id == 6) {
+            Kp = param;
+          }
+          serialState = 0;
+          break;
+      }
+      inputString = "";
+      /*Serial.print("desiredVit");
+      Serial.println(desiredVit[0]);
+      Serial.print("desiredPos");
+      Serial.println(desiredPos[0]);
+      Serial.print("state");
+      Serial.println(state[0]);*/
+      
+    }else{
+      inputString += inChar;
+    }
+  }
+  for (int imotor = 0; imotor <= 3; imotor++) {
+    // read lasers
+    lasers[imotor].read(false);
+    if (lasers[imotor].ranging_data.range_status == 0) {
+      rangemm[imotor] = lasers[imotor].ranging_data.range_mm;
+    } else {
+      rangemm[imotor] = 1000;
+    }
+    //print
+    /*Serial.print(imotor);
+    Serial.print(":");
+    Serial.print(realPos[imotor]);
+    Serial.print(";");
+    Serial.print(rangemm[imotor]);
+    Serial.println(".");*/
+    // choose speed
+    int v = 0;
+    switch (state[imotor]) {
+      case 2:
+        v = desiredVit[imotor];
+        break;
+      case 3:
+        error = realPos[imotor]-desiredPos[imotor];
+        errorInt+=error;
+        v = (int)(Kp*error+Ki*errorInt);
+        break;  
+      default:
+        v = 0;
+    }
+    // anti node protection
+    if(rangemm[imotor]<50 and v>0){
+      v = 0;
+    }
+    // limit speed
+    if (v > 0) {
+      if (v > Vmax) {
+        v = Vmax;
+      }
+      analogWrite(pinPWM[imotor], v);
+      digitalWrite(pinDir[imotor], LOW);
+    } else if (v < 0) {
+      if (v < -Vmax) {
+        v = -Vmax;
+      }
+      analogWrite(pinPWM[imotor], -v);
+      digitalWrite(pinDir[imotor], HIGH);
+    } else {
+      analogWrite(pinPWM[imotor], 0);
+      digitalWrite(pinDir[imotor], LOW);
+    }
+  }
+  delay(dtMs);
+}
+
+
+
+// INTERRUPTS
+void incDec1() {
+  if (digitalRead(CHA1) && !digitalRead(CHB1)) {
+    realPos[0]++;
+  }
+  if (digitalRead(CHA1) && digitalRead(CHB1)) {
+    realPos[0]--;
+  }
+}
+void incDec2() {
+  if (digitalRead(CHA2) && !digitalRead(CHB2)) {
+    realPos[1]++;
+  }
+  if (digitalRead(CHA2) && digitalRead(CHB2)) {
+    realPos[1]--;
+  }
+}
+void incDec3() {
+  if (digitalRead(CHA3) && !digitalRead(CHB3)) {
+    realPos[2]++;
+  }
+  if (digitalRead(CHA3) && digitalRead(CHB3)) {
+    realPos[2]--;
+  }
+}
+void incDec4() {
+  if (digitalRead(CHA4) && !digitalRead(CHB4)) {
+    realPos[3]++;
+  }
+  if (digitalRead(CHA4) && digitalRead(CHB4)) {
+    realPos[3]--;
+  }
+}
